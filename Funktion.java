1df70f88d5304ba77dedddf2cf08e240d00de1bd
Cypeg <44675372+Cypeg@users.noreply.github.com>
2018-12-10 14:20:58 +0100

    Test mit ßẞ hinzugefügt und einige Kommentare

--- Funktion.java
+++ Funktion.java
@@ -1,7 +1,7 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 /**
- *  
+ *
  *  Klasse Funktion (Arithmetisches Mittel, Anzahl reiner Textstrings, Array sortieren)
  * @author JKrier, JVogt
  * @version Ueb06
@@ -13,66 +13,71 @@ public class Funktion{
    */
     private Funktion(){
   }
- 
+
   /**
-   *  berechneWerte berechnet das Arithmetische Mittel eines Arrays sowie den Messwert, der am nächsten daran liegt und den der am weitesten davon entfernt ist. 
-   * 
+   *  berechneWerte berechnet das Arithmetische Mittel eines Arrays sowie den Messwert, der am nächsten daran liegt und den der am weitesten davon entfernt ist.
+   *
    * @param double Array, fuer das die Werte berechnet werden
    * @return  MinWert, MittelWert, MaxWert
    */
    public static double[] berechneWerte(double array[] ){
-       
+
         double min = array[0];
         double mittel = 0.0;
         double max = array[0];
-        
-        for (int i = 1; i < array.length; i++){
+                                                          // ich hab das mit 3 for schleifen gemacht, min mittel max
+        for (int i = 0; i < array.length; i++){           // ich habe bei jeder for schleife i = 0 , gibt es einen grund für i = 1?
+
+            // Min bestimmen
             if (Math.abs(mittel - array[i]) < Math.abs(mittel - min)){
                  min = array[i];
             }
 
-            if ( Math.abs(mittel - array[i]) > (Math.abs(mittel - max))){
+            // Max bestimmen
+            if (Math.abs(mittel - array[i]) > (Math.abs(mittel - max))){
                 max = array[i];
             }
         }
-           
+
+        // Mittelwert bestimmen
         for (int i = 0; i < array.length; i++){
-           mittel += array[i];
+            mittel += array[i];
         }
+
         mittel /= array.length;
-       
+
         return new double[] { min, mittel, max };
     }
-    
+
   /**
    *  berechneAnzahl - gibt die Anzahl reiner Textstrings eines Arrays an
-   *  
+   *
    * @param  eingabeString   der zu untersuchende String
-   * @return        true ==  enthaelt nur Buchstaben, false == enthaelt NICHT nur Buchstaben
+   * @return        true == enthaelt nur Buchstaben, false == enthaelt NICHT nur Buchstaben
    */
    public static int berechneAnzahl(String[] array){
        int anzahl = array.length;
        //Pattern mit den erlaubten Zeichen
-       for (int i = 0; i<array.length;i++){
-           String pattern =  "[a-z,A-Z,ä-ü,Ä-Ü,ß]*";
+       for (int i = 0; i<array.length; i++){
+           String pattern =  "[a-z,A-Z,ä-ü,Ä-Ü,ß,ẞ]*"; // das zweite scharfe s ist das "große" ß
            Pattern p = Pattern.compile(pattern);
            Matcher m = p.matcher(array[i]);
            boolean b = m.matches();
-           if( b == false){
+           if(b == false){
                anzahl--;
             }
-        }   
+        }
        return anzahl;
-       
+
     }
 
 
-    
-   
+
+
   /**
    *  insertionSort - Sortiert das uebergebene int-Array
    *
-   * @param  das Array, das sortiert werden soll
+   * @param das Array, das sortiert werden soll
    */
    public static void insertionSort(int[] array){
      int temp;
@@ -86,5 +91,5 @@ public class Funktion{
             array[j] = temp;
         }
     }
-    
-} 
+
+}
--- FunktionTest.java
+++ FunktionTest.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 import java.util.Scanner;
-    
+
 /**
  *    Klasse FunktionTest
  *
@@ -12,31 +12,31 @@ import java.util.Scanner;
 
  public class FunktionTest{
     private Scanner input;
-    
-    private static final int WERTE = 1;       
-    private static final int ANZAHL = 2; 
+
+    private static final int WERTE =  1;
+    private static final int ANZAHL = 2;
     private static final int SORT   = 3;
     private static final int ENDE   = 0;
-    
+
     private static final String INT_WERT = "Hier bitte eine ganze Zahl eingeben!";
-    
+
    /**
     * Standard-Konstruktor fuer TestMyUeb6Methods
     */
    public FunktionTest(){
        input = new Scanner(System.in);
    }
-      
+
    /**
     * run --> laesst die MyUeb6Methods-Test's ablaufen
-    * 
+    *
     */
    public void run(){
          int wahl = 1;
 
          while (wahl != ENDE){
             try{
-                 wahl = readlnInt("\n\n Auswahl: \n 1 = Artithmetisches Mittel berechnen \n 2 = Anzahl reiner Text Strings\n 3 = Inerstion Sort"
+                 wahl = readlnInt("\n\n Auswahl: \n 1 = Artithmetisches Mittel berechnen \n 2 = Anzahl reiner Text Strings\n 3 = Insertion Sort"
                                  +"\n 0 =  Ende\n");
                  switch (wahl){
                     case WERTE :
@@ -54,22 +54,22 @@ import java.util.Scanner;
                     default :
                          System.out.println("\nTreffen Sie bitte eine gueltige Auswahl!\n");
                          break;
-                   }   
+                   }
             }
             catch (AssertionError ae){
                 System.err.println(ae);
             }
             catch ( NumberFormatException nfe){
-                System.err.println(nfe);                           
-            }       
+                System.err.println(nfe);
+            }
             catch (RuntimeException rex){
                 System.err.println(rex);
             }
-            
-         } 
-    
+
+         }
+
    }
-    
+
    /**
     *  testeMittel: testet die Funktion berechneMittel der Klasse Funktion
     *
@@ -80,31 +80,32 @@ import java.util.Scanner;
       double testArray[][] = { { -10.0, 20.0, -0.4, 15.35, 23.23, 98.45, 10.11, -123.0, 12.4, 20.0,},
                                { 8.0, 2.0, -15.0, 14.0, -12.0, 1.0, 77.7, -34.5, 0.1, -0.15, -12.3 },
                                { 1.1, -2.2, 3.3, -4.4, 5.5, -6.6, 7.7, -8.8, 9.9, -10.10, 0.0 }};
-      
+
       for (int i = 0; i < arrayAnzahl; i++ ){
            System.out.println( "Testarray " + (i+1)+ ": " + java.util.Arrays.toString(testArray[i]) +
                                "\nDie Ergebnisse sind: "+ java.util.Arrays.toString(Funktion.berechneWerte(testArray[i])));
       }
    }
-  
+
    /**
     *  testeAnzahl: testet die Funktion berechneAnzahl der Klasse Funktion
     *
     */
     private void testeAnzahl(){
-        int arrayAnzahl = 4;
-        
+        int arrayAnzahl = 5;
+
         String[][] testArray = {{"Test", "AbCde", "Polizei", "NIX", "ProgI"},
                               {"Test", "ABcdE", "12Polizei", "!?\\", "Prog1"},
                               {"Test", "ab cd e", "1Polizei2", "N!x", "Pr0gI"},
-                              {"tesT", "a b c", " Pol ", "1234", "text12!"}};
-                              
-       for ( int i = 0; i < arrayAnzahl; i++ ){
+                              {"tesT", "a b c", " Pol ", "1234", "text12!"}, // wieso wird hier nur "1" ausgegeben? Das müsste doch "2" sein oder?
+                              {"tEsT", "aBCDe", "Polizei", "ẞß", "Programmierung1"}}; // ßẞ implementiert, zählt mit
+
+       for (int i = 0; i < arrayAnzahl; i++ ){
           System.out.println("Testarray " + (i+1)+ ": " + java.util.Arrays.toString(testArray[i]) +
                              "\nAnzahl reiner Buchstaben-Strings: " + Funktion.berechneAnzahl(testArray[i]));
       }
    }
-    
+
    /**
     *  testeSort: testet die Funktion insertionSort der Klasse Funktion
     *
@@ -113,24 +114,24 @@ import java.util.Scanner;
         int [] test1 = {10,30,50,80,90,50,10,70,60,0};
         int [] test2 = {55, 77, 99, 11, 44, 33, 22, 66, 88, 111};
         int [] test3 = {73,43,53,23,3,83,63,13,93,33};
-     
+
          System.out.println("Testarray 1: \t" + java.util.Arrays.toString(test1));
          Funktion.insertionSort(test1);
          System.out.println("\nSortiert: \t" + java.util.Arrays.toString(test1));
-    
+
          System.out.println("\nTestarray 2: \t" + java.util.Arrays.toString(test2));
          Funktion.insertionSort(test2);
          System.out.println("\nSortiert: \t" + java.util.Arrays.toString(test2));
-   
+
          System.out.println("\nTestarray 3: \t" + java.util.Arrays.toString(test3));
          Funktion.insertionSort(test3);
          System.out.println("\nSortiert: \t" + java.util.Arrays.toString(test3));
-     
+
    }
-   
+
    /**
      * Einlesen einzugebener Integer Werte.
-     *  
+     *
      *  @param  Eingabeaufforderung Der Eingabe-Aufforderungs-Text
      *  @return die Eingabe-Zahl
      */
@@ -143,14 +144,14 @@ import java.util.Scanner;
         }
         catch (InputMismatchException e){
             System.err.println(e+ INT_WERT);
-        } 
+        }
         return wert;
    }
-    
+
    /**
      *  main Methode Ausfuehren des interaktiven Tests.
      *  @param args Kommandozeilenparameter
-     */  
+     */
     public static void main(String[] args){
         FunktionTest test = new FunktionTest();
         test.run();
